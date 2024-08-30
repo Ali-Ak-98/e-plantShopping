1e6d59478840c610b341dd5879601a39ad97555c
Ali Akbarzadeh <ali1998akbarzadeh@gmail.com>
2024-08-30 09:27:49 +0200

    fix project

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,23 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
+import {createFactory} from "react";
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            state.items = [...state.items, action.payload]
+        },
+        removeItem: (state, action) => {
+            let index = state.items.findIndex(e => e.name === action.payload.name)
+            state.items.splice(index, 1)
+        },
+        updateQuantity: (state, action) => {
+            let index = state.items.findIndex(e => e.name === action.payload.name)
+            state.items[index] = action.payload
+        },
     },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export const {addItem, removeItem, updateQuantity} = CartSlice.actions;
 
 export default CartSlice.reducer;
